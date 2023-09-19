c2751a74ebc0cadd13488d152e1f6a26aa15832f
Keerthana071 <keerthanaselvakumar31@gmail.com>
2023-09-19 15:27:01 +0530

    Dropdown

---
+++ NAFSupport.Server/Migrations/20230919065727_migsupport.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NAFSupport.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class migsupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "department",
+                columns: table => new
+                {
+                    departmentRefId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    departmentName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_department", x => x.departmentRefId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "role",
+                columns: table => new
+                {
+                    roleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    roleName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role", x => x.roleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ticketId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    createdBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    assignedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    startDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    endDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    priority = table.Column<int>(type: "int", nullable: false),
+                    severity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ticketStatus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ticketStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userDetails",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    userName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    role = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    roleId = table.Column<int>(type: "int", nullable: true),
+                    department = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    departmentRefId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userDetails", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "department");
+
+            migrationBuilder.DropTable(
+                name: "role");
+
+            migrationBuilder.DropTable(
+                name: "tickets");
+
+            migrationBuilder.DropTable(
+                name: "ticketStatus");
+
+            migrationBuilder.DropTable(
+                name: "userDetails");
+        }
+    }
+}
